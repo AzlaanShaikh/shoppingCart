e3f56969a47c21b60264ffe782f2534a88fbadd0
Azlaan Shaikh <115040817+AzlaanShaikh@users.noreply.github.com>
2024-09-09 16:05:28 +0530

    Added backend

---
+++ backend/routes/product.routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { createProduct, deleteProduct, getProduct, updatedProduct } from "../controller/product.controller.js";
+
+const router = express.Router();
+
+router.get("/",getProduct)
+
+router.post("/" , createProduct)
+
+router.put("/:id" ,updatedProduct)
+
+router.delete("/:id", deleteProduct)
+
+export default router;
